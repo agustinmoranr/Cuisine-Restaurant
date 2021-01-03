de481de6b6752f1242d5dca9f64390cdab6f077c
agustin <agustin.moranr@gmail.com>
2021-01-03 14:31:03 -0600

    [ADD] funcionamiento principal del flier de videos

---
+++ src/js/index.js
@@ -0,0 +1,66 @@
+const ORDER_NOW_VIDEOS = [
+  'shakes.mp4',
+  'salad.mp4', 
+  'tarta.mp4'
+];
+
+const NEXT_BTN = document.getElementById('next-btn')
+const PREV_BTN = document.getElementById('prev-btn')
+const PLAY_BTN = document.getElementById('ordnow-media-play-btn')
+const ORD_NOW_MEDIA = document.querySelector('.ordnow-media');
+
+let i = 0;
+
+function nextSlide() {
+  if(i >= ORDER_NOW_VIDEOS.length -1) {
+    i = 0;
+    return setVideoSource(i)
+  }
+
+  i = i + 1;
+
+  return setVideoSource(i)
+}
+
+function prevSlide() {
+  if(i <= 0) {
+    i = ORDER_NOW_VIDEOS.length -1;
+    return setVideoSource(i)
+  }
+
+  i = i - 1;
+
+  return setVideoSource(i)
+}
+
+function playMedia() {
+  
+  return ORD_NOW_MEDIA.play()
+}
+
+function setElementAttribute(element, attribute, value) {
+  element.setAttribute(attribute, value)
+};
+
+function setElementChild (parentElement, childElement) {
+  parentElement.appendChild(childElement)
+}
+
+function setVideoSource(videoIndex) {
+  const SOURCE = document.createElement('source');
+  
+  const PREV_SOURCE = document.getElementsByClassName('ordnow-media-source')[0];
+  
+  if(typeof(PREV_SOURCE) !== "undefined") {
+    PREV_SOURCE.remove()
+  } 
+  
+  setElementAttribute(SOURCE, 'class', 'ordnow-media-source');
+  setElementAttribute(SOURCE, 'src', `./assets/videos/${ORDER_NOW_VIDEOS[videoIndex]}`);
+  setElementAttribute(SOURCE, 'alt', 'video de muestra de la cocina de un restaurante');
+  
+  setElementChild(ORD_NOW_MEDIA, SOURCE)
+  
+  ORD_NOW_MEDIA.load()
+}
+
