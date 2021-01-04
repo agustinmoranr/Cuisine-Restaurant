d3c3f04ae728f2de730b22e4152a1c3df01af9d6
agustin <agustin.moranr@gmail.com>
2021-01-03 20:14:22 -0600

    [ADD] seccion de order now v1

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,8 @@ const ORDER_NOW_VIDEOS = [
 const NEXT_BTN = document.getElementById('next-btn')
 const PREV_BTN = document.getElementById('prev-btn')
 const PLAY_BTN = document.getElementById('ordnow-media-play-btn')
+
+const MEDIA_CTROLS = document.querySelector('.order-now-media-ctrols');
 const ORD_NOW_MEDIA = document.querySelector('.ordnow-media');
 
 let i = 0;
@@ -33,10 +35,24 @@ function prevSlide() {
   return setVideoSource(i)
 }
 
+ORD_NOW_MEDIA.onloadstart = function(e) {
+  e.preventDefault()
+  PLAY_BTN.style.display = 'block';
+}
 function playMedia() {
-  
   return ORD_NOW_MEDIA.play()
 }
+ORD_NOW_MEDIA.addEventListener('play', (e) => {
+  e.preventDefault()
+  PLAY_BTN.style.display = 'none';
+})
+ORD_NOW_MEDIA.addEventListener('pause', (e) => {
+  PLAY_BTN.style.display = 'block';
+})
+ORD_NOW_MEDIA.addEventListener('ended', (e) => {
+  nextSlide();
+})
+
 
 function setElementAttribute(element, attribute, value) {
   element.setAttribute(attribute, value)
@@ -48,12 +64,12 @@ function setElementChild (parentElement, childElement) {
 
 function setVideoSource(videoIndex) {
   const SOURCE = document.createElement('source');
-  
+
   const PREV_SOURCE = document.getElementsByClassName('ordnow-media-source')[0];
   
   if(typeof(PREV_SOURCE) !== "undefined") {
     PREV_SOURCE.remove()
-  } 
+  }
   
   setElementAttribute(SOURCE, 'class', 'ordnow-media-source');
   setElementAttribute(SOURCE, 'src', `./assets/videos/${ORDER_NOW_VIDEOS[videoIndex]}`);
