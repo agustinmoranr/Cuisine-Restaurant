85ecb464c1c9fe3670048ee3b8c68221dbe98d4d
agustin <agustin.moranr@gmail.com>
2021-01-07 03:31:46 -0600

    [Edit] Refactor de la seccion menu, ahora usamos javascript para modificar el contenido y no duplicarlo. Eliminando 1000 lineas de html y 100 de css

--- src/js/index.js
+++ src/js/index.js
@@ -90,6 +90,361 @@
 // order now section end
 
 
+
+
+
+/* --------order-now section start-------- */
+// const menu = {
+//   'breakfast': {
+//     '0': [
+//       {
+//         id:0,
+//         img: 'hcakes.jpg',
+//         price: "$49",
+//         name: "Hot Cakes"
+//       },
+//       {
+//         id:1,
+//         img: 'hcakes.jpg',
+//         price: "$49",
+//         name: "Hot Cakes"
+//       }
+//     ],
+//     '1': [
+//       {
+//         id:0,
+//         img: 'hcakes.jpg',
+//         price: "$49",
+//         name: "Hot Cakes"
+//       },
+//       {
+//         id:1,
+//         img: 'hcakes.jpg',
+//         price: "$49",
+//         name: "Hot Cakes"
+//       }
+//     ],
+//     '2': [
+//       {
+//         id:0,
+//         img: 'hcakes.jpg',
+//         price: "$49",
+//         name: "Hot Cakes"
+//       },
+//       {
+//         id:1,
+//         img: 'hcakes.jpg',
+//         price: "$49",
+//         name: "Hot Cakes"
+//       }
+//     ],
+//   }
+// }
+const MENU = {
+  'breakfast': [
+    {
+      id: 0,
+      cards: [
+        {
+          id:0,
+          img: 'breakfast/berry-cereal.webp',
+          price: "$49",
+          name: "Berry Cereal"
+        },
+        {
+          id:1,
+          img: 'breakfast/hcakes.jpg',
+          price: "$49",
+          name: "Hot Cakes"
+        },
+      ]
+    },
+    {
+      id: 1,
+      cards: [
+        {
+          id:0,
+          img: 'breakfast/eggs.jpg',
+          price: "$49",
+          name: "Eggs"
+        },
+        {
+          id:1,
+          img: 'breakfast/sandwich.jpg',
+          price: "$39",
+          name: "Sandwich"
+        },
+      ]
+    },
+    {
+      id: 2,
+      cards: [
+        {
+          id:0,
+          img: 'breakfast/spaguetti.jpg',
+          price: "$89",
+          name: "Spaguetti"
+        },
+        {
+          id:1,
+          img: 'breakfast/coditos.jpg',
+          price: "$89",
+          name: "Coditos"
+        }
+      ]
+    }
+  ],
+  'lunch': [
+    {
+      id: 0,
+      cards: [
+        {
+          id:0,
+          img: 'lunch/mojarra.jpg',
+          price: "$139",
+          name: "Mojarra"
+        },
+        {
+          id:1,
+          img: 'lunch/meal.jpg',
+          price: "$119",
+          name: "Barbecue tenderloin"
+        },
+      ]
+    },
+    {
+      id: 1,
+      cards: [
+        {
+          id:0,
+          img: 'lunch/hamburger.jpg',
+          price: "$99",
+          name: "Hamburger"
+        },
+        {
+          id:1,
+          img: 'lunch/burritos.jpg',
+          price: "$99",
+          name: "Burritos"
+        },
+      ]
+    },
+    {
+      id: 2,
+      cards: [
+        {
+          id:0,
+          img: 'lunch/pizza.jpg',
+          price: "$179",
+          name: "Pizza Grande"
+        },
+        {
+          id:1,
+          img: 'lunch/fideo-soup.jpg',
+          price: "$49",
+          name: "Fideo Soup"
+        }
+      ]
+    }
+  ],
+  'dinner': [
+    {
+      id: 0,
+      cards: [
+        {
+          id:0,
+          img: 'dinner/salmon.jpg',
+          price: "$149",
+          name: "Salmón"
+        },
+        {
+          id:1,
+          img: 'dinner/brocheta.jpg',
+          price: "$109",
+          name: "Meat Skewer"
+        },
+      ]
+    },
+    {
+      id: 1,
+      cards: [
+        {
+          id:0,
+          img: 'dinner/albondigas.jpg',
+          price: "$89",
+          name: "Meatloaf"
+        },
+        {
+          id:1,
+          img: 'dinner/chicken.jpg',
+          price: "$99",
+          name: "Chicken Breast"
+        },
+      ]
+    },
+    {
+      id: 2,
+      cards: [
+        {
+          id:0,
+          img: 'dinner/tacos.jpg',
+          price: "$69",
+          name: "Tacos"
+        },
+        {
+          id:1,
+          img: 'dinner/bistec.jpg',
+          price: "$89",
+          name: "Steak"
+        }
+      ]
+    }
+  ],
+  'dessert': [
+    {
+      id: 0,
+      cards: [
+        {
+          id:0,
+          img: 'dessert/cake.jpg',
+          price: "$59",
+          name: "Cake"
+        },
+        {
+          id:1,
+          img: 'dessert/cupcake.jpg',
+          price: "$59",
+          name: "Cupcake"
+        },
+      ]
+    },
+    {
+      id: 1,
+      cards: [
+        {
+          id:0,
+          img: 'dessert/ice-cream.jpg',
+          price: "$49",
+          name: "Ice Cream"
+        },
+        {
+          id:1,
+          img: 'dessert/popsicle.jpg',
+          price: "$49",
+          name: "Popsicle"
+        },
+      ]
+    },
+  ],
+  'shakes': [
+    {
+      id: 0,
+      cards: [
+        {
+          id:0,
+          img: 'shakes/candy-frappe.jpg',
+          price: "$59",
+          name: "Candy Frappe"
+        },
+        {
+          id:1,
+          img: 'shakes/frappuchino-coffe.jpg',
+          price: "$59",
+          name: "Frappuchino Coffee"
+        },
+      ]
+    },
+    {
+      id: 1,
+      cards: [
+        {
+          id:0,
+          img: 'shakes/strawberry-milkshake.jpg',
+          price: "$49",
+          name: "Strawberry Milkshake"
+        },
+        {
+          id:1,
+          img: 'shakes/choco-milkshake.jpg',
+          price: "$49",
+          name: "Choco Milkshake"
+        },
+      ]
+    },
+    
+  ],
+}
+
+const MENU_SLIDER = document.querySelector("#menu").querySelector('.slider')
+const MENU_SLIDE = MENU_SLIDER.querySelector('.slide')
+const MENU_SLIDES_CTROLS = MENU_SLIDER.querySelector('.manual-ctrls')
+
+const ARR_FIRST_CARDS_IMG = Array.from(MENU_SLIDE.getElementsByClassName('first-img'))
+const ARR_FIRST_CARDS_PRICES = Array.from(MENU_SLIDE.getElementsByClassName('first-price'))
+const ARR_FIRST_CARDS_NAMES = Array.from(MENU_SLIDE.getElementsByClassName('first-name'))
+
+const ARR_SECOND_CARDS_IMG = Array.from(MENU_SLIDE.getElementsByClassName('second-img'))
+const ARR_SECOND_CARDS_PRICES = Array.from(MENU_SLIDE.getElementsByClassName('second-price'))
+const ARR_SECOND_CARDS_NAMES = Array.from(MENU_SLIDE.getElementsByClassName('second-name'))
+
+
+function HandleMenuSlide(data, id) {
+  const CARDS_FIRST = data[id].cards[0]
+  const CARDS_FIRST_IMG = `./assets/img/menu/${CARDS_FIRST.img}`
+  const CARDS_SECOND = data[id].cards[1]
+  const CARDS_SECOND_IMG = `./assets/img/menu/${CARDS_SECOND.img}`
+
+  HandleCardInfo(ARR_SECOND_CARDS_IMG, 'setAttribute', 'src', CARDS_SECOND_IMG)
+  HandleCardInfo(ARR_SECOND_CARDS_PRICES, 'innerText', '', CARDS_SECOND.price)
+  HandleCardInfo(ARR_SECOND_CARDS_NAMES, 'innerText', '', CARDS_SECOND.name)
+
+  HandleCardInfo(ARR_FIRST_CARDS_IMG, 'setAttribute', 'src', CARDS_FIRST_IMG)
+  HandleCardInfo(ARR_FIRST_CARDS_PRICES, 'innerText', '', CARDS_FIRST.price)
+  HandleCardInfo(ARR_FIRST_CARDS_NAMES, 'innerText', '', CARDS_FIRST.name)
+}
+
+
+function HandleCardInfo(array, metodo, attribute, newValue) {
+  switch (metodo) {
+    case 'innerText': 
+      array.forEach(element => element.innerText = newValue)
+    break;
+    case 'setAttribute': 
+      array.forEach(element => setElementAttribute(element, attribute, newValue))
+  }
+}
+
+function handleMenuCategory(category) {
+  MENU_SLIDES_CTROLS.innerHTML = ''
+  MENU_SLIDER.querySelectorAll('.slider-selector').forEach(selector => selector.remove());
+
+  createSliderControls(MENU, 'menu', MENU_SLIDER, MENU_SLIDES_CTROLS, category)
+
+  HandleMenuSlide(MENU[category], 0)
+}
+
+function handleAddToCart(element) {
+  const button = element.querySelector('.card-buy-btn')
+  
+    button.innerText = 'Added Succesfully'
+    button.style.backgroundColor = 'green'
+    element.style.display = 'grid'
+
+    setTimeout(() => {
+      button.innerText = 'Order Now'
+      button.style.backgroundColor = '#ff8800'
+    }, 2000)
+}
+/* --------order-now section end-------- */
+
+
+
+
+
+
+
+
+
 // blog section start
   const REVIEWS = [
     {
@@ -122,29 +477,40 @@
   const BLOG = document.querySelector(".blog")
   const BLOG_SLIDER_CTROLS =  REVIEWS_SLIDER.querySelector('.blog-ctrols')
 
-  function createSliderControls(elementArray, section, inputsParentEl, labelsParentEl) {
+  function createSliderControls(elementArray, section, inputsParentEl, labelsParentEl, menuCategory) {
+    let MockAPI;
+    menuCategory? MockAPI = elementArray[menuCategory]
+    : MockAPI = elementArray
 
-    elementArray.forEach(async element => {
+    MockAPI.forEach(async element => {
       //create input
-      let inputHandler = document.createElement('input')
+      let newInput = document.createElement('input')
       //define position and id
       let id = element.id
       let inputId = `slide-${section}-${id}`
+
+      let extraInfo = {
+        'blog': ['radio', 'blog-select-slide', `changeBlogData(REVIEWS, ${id})`],
+        'menu': ['radio', 'menu-select-slide', `HandleMenuSlide(MENU['${menuCategory}'], ${id})`],
+      }
+      let inputType = extraInfo[section][0]
+      let inputName = extraInfo[section][1]
+      let inputHandler = extraInfo[section][2]
       
       //first input should-be checked by default 
       if(id === 0) {
-        setElementAttribute(inputHandler, 'checked', '')
+        setElementAttribute(newInput, 'checked', '')
       }
 
       //define all its attributes
-      setElementAttribute(inputHandler, 'type', 'radio')
-      setElementAttribute(inputHandler, 'name', 'blog-select-slider')
-      setElementAttribute(inputHandler, 'id', inputId)
-      setElementAttribute(inputHandler, 'class', 'slider-selector')
-      setElementAttribute(inputHandler, 'onclick',`changeBlogData(REVIEWS, ${id})`)
+      setElementAttribute(newInput, 'type', inputType)
+      setElementAttribute(newInput, 'name', inputName)
+      setElementAttribute(newInput, 'id', inputId)
+      setElementAttribute(newInput, 'class', 'slider-selector')
+      setElementAttribute(newInput, 'onclick', inputHandler)
 
       //Set inputs into blog
-      setElementChildBefore(inputsParentEl, inputHandler);
+      setElementChildBefore(inputsParentEl, newInput);
 
       //Set labels into section controls
       createLabelsHandlers(labelsParentEl, inputId)
@@ -179,5 +545,6 @@
     REVIEWER_PROFESION.innerText = DATA.profesion
   }
 
-  window.onload = createSliderControls(REVIEWS, 'blog', BLOG, BLOG_SLIDER_CTROLS)
+  window.onload = createSliderControls(REVIEWS, 'blog', BLOG, BLOG_SLIDER_CTROLS, '')
+  window.onload = createSliderControls(MENU, 'menu', MENU_SLIDER, MENU_SLIDES_CTROLS, 'breakfast')
 // blog section end
